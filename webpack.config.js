c6e4bde575b0a99c252927dee27b7a14c18d44b5
Dylan Huang <dylan.p.huang@gmail.com>
2019-07-21 15:53:42 -0500

    Remove options from webpack.config.js and just use .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,6 @@ const config = {
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
         },
       },
     ],
